34074ff19bfde0d0267332ba3adaddea61ee5f01
butenuth <butenuth@butenuth>
2026-03-16 14:23:40 +0100

    Added tests with wait and retries exhausted

--- src/test/java/de/codecentric/limiter/api/Handle429Tests.java
+++ src/test/java/de/codecentric/limiter/api/Handle429Tests.java
@@ -3,41 +3,6 @@ package de.codecentric.limiter.api;
 public class Handle429Tests {
 
 
-//	public void notANumberButString() throws Exception {
-//		TestHttpResponseAttributesFactory.addEntry(429, "retry-after-ms", "100");
-//		Exception exception = flowRunner("test-wait-time-not-a-number-but-string").runExpectingException();
-//		assertTrue("INVALID_NUMBER not found", exception.getCause().getMessage().contains("INVALID_NUMBER"));
-//	}
-
-//	public void notANumberButObject() throws Exception {
-//		TestHttpResponseAttributesFactory.addEntry(429, "retry-after-ms", "100");
-//		Exception exception = flowRunner("test-wait-time-not-a-number-but-object").runExpectingException();
-//		assertTrue("INVALID_NUMBER not found", exception.getCause().getMessage().contains("INVALID_NUMBER"));
-//	}
-
-//	public void noWaitnecessary() throws Exception {
-//		TestHttpResponseAttributesFactory.addEntry(200);
-//		Event event = flowRunner("test-no-wait").run();
-//		@SuppressWarnings("unchecked")
-//		Map<String, Object> payload = (Map<String, Object>) event.getMessage().getPayload().getValue();
-//		long a = (long) payload.get("a");
-//		long b = (long) payload.get("b");
-//		// a to b should be immediately, but can be delayed because some Mule parts have to be initialized
-//		assertRange("a -> b", b - a, 0, 90);
-//	}
-
-//	public void oneWaitnecessary() throws Exception {
-//		TestHttpResponseAttributesFactory.addEntry(429, "retry-after-ms", "100");
-//		TestHttpResponseAttributesFactory.addEntry(200);
-//		Event event = flowRunner("test-one-wait").run();
-//		@SuppressWarnings("unchecked")
-//		Map<String, Object> payload = (Map<String, Object>) event.getMessage().getPayload().getValue();
-//		long a = (long) payload.get("a");
-//		long b = (long) payload.get("b");
-//		// Wait time is 100 ms
-//		// a to b should be immediately, but can be delayed because some Mule parts have to be initialized
-//		assertRange("a -> b", b - a, 100, 1200); // long time, sometimes test seems to run slow
-//	}
 	
 //	public void retriesExhausted() throws Exception {
 //		TestHttpResponseAttributesFactory.addEntry(429, "retry-after-ms", "10");
@@ -46,31 +11,4 @@ public class Handle429Tests {
 //		assertTrue("RETRIES_EXHAUSTED not found", exception.getCause().getMessage().contains("RETRIES_EXHAUSTED"));
 //	}
 
-//	@SuppressWarnings("unchecked")
-//	public void waitBeforeFirstAccess() throws Exception {
-//		TestHttpResponseAttributesFactory.addEntry(429, "retry-after-ms", "100");
-//		TestHttpResponseAttributesFactory.addEntry(429, "retry-after-ms", "120");
-//		TestHttpResponseAttributesFactory.addEntry(429, "retry-after-ms", "100");
-//		TestHttpResponseAttributesFactory.addEntry(429, "retry-after-ms", "120");
-//		TestHttpResponseAttributesFactory.addEntry(200);
-//		TestHttpResponseAttributesFactory.addEntry(200);
-//		TestHttpResponseAttributesFactory.addEntry(200);
-//		TestHttpResponseAttributesFactory.addEntry(200);
-//		Event event = flowRunner("test-wait-before-first-access").run();
-//		Map<String, Object> payload = (Map<String, Object>) event.getMessage().getPayload().getValue();
-//		Map<String, Object>  route0 = (Map<String, Object>) ((Message) payload.get("0")).getPayload().getValue();
-//		Map<String, Object>  route1 = (Map<String, Object>) ((Message) payload.get("1")).getPayload().getValue();
-//		
-//		// Wait time is 100 ms
-//		long a = (long) route0.get("a");
-//		long b = (long) route0.get("b");
-//		long c = (long) route1.get("c");
-//		long d = (long) route1.get("d");
-//		
-//		System.out.println("a -> b " + (b - a));
-//		System.out.println("c -> d " + (d - c));
-//		
-//		assertRange("a -> b", b - a, 200, 490);
-//		assertRange("c -> d", d - c, 200, 490);
-//	}
 }
